2c36ff70d2c5aa970bc1940609cbe3e4d8ca043f
PulkitG0 <129782952+PulkitG0@users.noreply.github.com>
2023-10-26 14:50:10 +0530

    Update MINPIZZA.cpp

--- MINPIZZA.cpp
+++ MINPIZZA.cpp
@@ -5,9 +5,10 @@ int main() {
 	int t;
 	cin>>t;
 	while(t--){
-	    int a,b;
-	    cin>>a>>b;
-	    cout<<a+b<<"\n";
+	    int n,x;
+	    cin>>n>>x;
+	    if((n*x)%4==0) cout<<(n*x)/4<<"\n";
+	    else cout<<((n*x)/4)+1<<"\n";
 	}
 	return 0;
 }
