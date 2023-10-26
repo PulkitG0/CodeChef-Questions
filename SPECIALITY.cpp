b52ead804e8bd4157f8f51ae3d4ac42474062b85
PulkitG0 <129782952+PulkitG0@users.noreply.github.com>
2023-10-26 14:48:10 +0530

    Create SPECIALITY.cpp

---
+++ SPECIALITY.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        if(n>=67 && n<=45000) cout<<"YES \n";
+        else cout<<"NO \n";
+    }
+    return 0;
+}
