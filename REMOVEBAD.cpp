fa5fe8da50a9efff3e8a9ca22a5003e0934ef3e9
PulkitG0 <129782952+PulkitG0@users.noreply.github.com>
2023-10-26 14:41:49 +0530

    Create REMOVEBAD.cpp

---
+++ REMOVEBAD.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n, max=0;
+        cin>>n;
+        vector<int>v(n);
+        for(int i=0;i<n;i++) cin>>v[i];
+        sort(v.begin(), v.end());
+        int count =1;
+        for(int i=1;i<n;i++){
+            if(v[i]==v[i-1]) count++;
+            if(v[i]!=v[i-1] || i==n-1){
+                if(count>max) max=count;
+                count=1;
+            }
+        }
+        cout<<n-max<<"\n";
+    }
+    return 0;
+}
