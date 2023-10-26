48ffbd7aac6963177680a15e4b840a6483750b94
PulkitG0 <129782952+PulkitG0@users.noreply.github.com>
2023-10-26 14:52:53 +0530

    Create EXPERT.cpp

---
+++ EXPERT.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long double
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        ll x,y;
+        cin>>x>>y;
+        if(2*y>=x) cout<<"yes \n";
+        else cout<<"no \n";
+        
+    }
+    return 0;
+}
